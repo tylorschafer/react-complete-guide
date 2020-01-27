bb6a5fb13662ed0f453f9b230b97d0ea510a481d
Tylor Schafer <tylorschafer@gmail.com>
2020-01-26 22:03:35 -0700

    Add onClick Handler to paragraph tag in Person Component

--- src/App.js
+++ src/App.js
@@ -25,9 +25,16 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p> This is Super Cool!</p>
         <button onClick={this.switchNameHandler}>Switch Names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
