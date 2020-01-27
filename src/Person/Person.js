d36c9c732d9b315efb98b6a5c1b8841915e3a27c
Tylor Schafer <tylorschafer@gmail.com>
2020-01-26 21:12:42 -0700

    Add props.children example to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const person = (props) => {
-  return <p>My name is {props.name}, and I am {props.age} years old!</p>
+  return (
+    <div>
+      <p>My name is {props.name}, and I am {props.age} years old!</p>
+      <p>{props.children}</p>
+    </div>
+  )
 }
 
 export default person
