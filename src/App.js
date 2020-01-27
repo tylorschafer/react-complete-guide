e8899b1a8e1ad7f9dfe5a28951765d8a09ba2b37
Tylor Schafer <tylorschafer@gmail.com>
2020-01-26 21:47:18 -0700

    Convert App class component to function component with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Tylor', age: 29 },
       { name: 'Blue', age: 8 },
       { name: 'Sara', age:27 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({ persons: [
+  const switchNameHandler = () => {
+    setPersonsState({ persons: [
       { name: 'Moe', age: 29 },
       { name: 'Joe', age: 8 },
       { name: 'Poe', age:27 }
     ] })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p> This is Super Cool!</p>
-        <button onClick={this.switchNameHandler}>Switch Names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p> This is Super Cool!</p>
+      <button onClick={switchNameHandler}>Switch Names</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
-export default App;
+export default app;
