da0dcbed0a5957d0df63f7cce5d9864bc484c0cf
Tylor Schafer <tylorschafer@gmail.com>
2020-01-26 21:54:05 -0700

    Rollback to old class bass component for App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Tylor', age: 29 },
       { name: 'Blue', age: 8 },
       { name: 'Sara', age:27 }
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
-    setPersonsState({ persons: [
+  switchNameHandler = () => {
+    this.setState({ persons: [
       { name: 'Moe', age: 29 },
       { name: 'Joe', age: 8 },
       { name: 'Poe', age:27 }
     ] })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p> This is Super Cool!</p>
-      <button onClick={switchNameHandler}>Switch Names</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p> This is Super Cool!</p>
+        <button onClick={this.switchNameHandler}>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
