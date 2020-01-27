f8abcd22e97a43c165669acda746bb1490a408f0
Tylor Schafer <tylorschafer@gmail.com>
2020-01-26 21:32:34 -0700

    Add click event handler to change state in Person component

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({ persons: [
+      { name: 'Moe', age: 29 },
+      { name: 'Joe', age: 8 },
+      { name: 'Poe', age:27 }
+    ] })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p> This is Super Cool!</p>
-        <button>Switch Person</button>
+        <button onClick={this.switchNameHandler}>Switch Names</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
