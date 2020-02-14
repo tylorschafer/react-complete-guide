f2d1da124b14ee3abd54d63479edb22391570fd2
Tylor Schafer <tylorschafer@gmail.com>
2020-02-13 19:57:35 -0700

    Refactor Person using styled-components package

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16 px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+        width: '450px';
+  }
+`;
 
 const person = (props) => {
   const style = {
@@ -8,11 +21,11 @@ const person = (props) => {
     }
   };
   return (
-    <div className='Person' style={style}>
+    <StyledDiv>
       <p onClick={props.click}>My name is {props.name}, and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name} />
-    </div>
+    </StyledDiv>
   )
 };
 
