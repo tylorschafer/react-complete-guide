ece5ad8a7a77c5d5f81c55c48f72ee6d6140483f
Tylor Schafer <tylorschafer@gmail.com>
2020-03-31 22:15:05 -0600

    Refactor App component into Cockpit component

---
+++ src/Components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  };
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold)
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm a React App</h1>
+      <p className={assignedClasses.join(' ')}> This is Super Cool!</p>
+      <button 
+        className={btnClass} 
+        onClick={props.clicked}> Show / Hide
+      </button> 
+    </div>
+  );
+};
+
+export default cockpit;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Person from './Person/Person'
+import ErrorBoundary from '../../Components/ErrorBoundary/ErrorBoundary'
 
 const persons = (props) => props.persons.map( (person, index) => {
     return <ErrorBoundary key = {person.id}><Person
